d688a0978c63c2cf2e0476108f4ecc5584dbac31
McMassiveNZ <12842622+McMassiveNZ@users.noreply.github.com>
2023-05-31 20:54:20 +0200

    Try CPM

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,17 +1,34 @@
 include_guard()
 
-include(FetchContent)
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # release-1.12.1
+# include(FetchContent)
+# FetchContent_Declare(
+# googletest
+# GIT_REPOSITORY https://github.com/google/googletest.git
+# GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # release-1.12.1
+# )
+# # For Windows: Prevent overriding the parent project's compiler/linker settings
+# set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+# FetchContent_MakeAvailable(googletest)
+# set_property(TARGET
+# gtest
+# gtest_main
+# gmock
+# gmock_main
+# PROPERTY FOLDER third_party/GoogleTest)
+
+CPMAddPackage(
+	NAME googletest
+	GITHUB_REPOSITORY google/googletest
+	GIT_TAG release-1.12.1
+	VERSION 1.12.1
+	OPTIONS
+	"INSTALL_GTEST OFF"
+	"gtest_force_shared_crt ON"
 )
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
-set_property(TARGET 
-	gtest 
-	gtest_main 
+
+set_property(TARGET
+	gtest
+	gtest_main
 	gmock
 	gmock_main
 	PROPERTY FOLDER third_party/GoogleTest)
